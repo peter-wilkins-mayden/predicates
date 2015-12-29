b3f97b54b1a32525f63df452025260ebad9b6e48
peter.wilkins.mayden <peter.wilkins@mayden.com>
2015-12-29 08:26:36 +0000

    first

--- src/predicates.clj
+++ src/predicates.clj
@@ -1,19 +1,37 @@
 (ns predicates)
 
 (defn sum-f [f g x]
-  :-)
+  (+ (f x)(g x)))
+
+(sum-f inc dec 4) ;=> 8
+(sum-f inc identity 5) ;=> 11
+(sum-f identity - 10) ;=> 0
 
 (defn less-than [n]
-  :-)
+  (fn [x] (> n x)))
 
 (defn equal-to [n]
-  :-)
+  (fn [x] (== n x)))
+
+(filter (less-than 3) [1 2 3 4 5])   ;=> (1 2)
+(filter (less-than 4) [-2 12 3 4 0]) ;=> (-2 3 0)
+(filter (equal-to 2) [2 1 3 2.0])    ;=> (2 2.0)
+(filter (equal-to 2) [3 4 5 6])      ;=> ()
 
 (defn set->predicate [a-set]
-  :-)
+  (fn [x] (contains? a-set x)))
+
+(filter (set->predicate #{1 2 3})     [0 2 4 6])       ;=> (2)
+(filter (set->predicate #{1 2 3 nil}) [2 nil 4 nil 6]) ;=> (2 nil nil)
 
 (defn pred-and [pred1 pred2]
-  :-)
+  (fn [x] (and (pred1 x)(pred2 x))))
+
+
+(filter (pred-and pos? even?) [1 2 -4 0 6 7 -3]) ;=> [2 6]
+(filter (pred-and pos? odd?) [1 2 -4 0 6 7 -3]) ;=> [1 7]
+(filter (pred-and (complement nil?) empty?) [[] '() nil {} #{}])
+;=> [[] '() {} #{}]
 
 (defn pred-or [pred1 pred2]
   :-)
